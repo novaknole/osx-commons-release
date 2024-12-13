be52712f7dcd8540635cd94ee2b55704a79b0e3a
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-13 14:42:08 +0400

    changes

--- test.sh
+++ test.sh
@@ -9,23 +9,26 @@ organization=@glagh
 # Define the mapping as an associative array
 mapping="contracts:commons-contracts configs:commons-configs sdk:commons-sdk artifacts:dev-artifacts"
 
-package="${REF_NAME##*-}"
+package="sdk"
 
-echo $REF_NAME
+# echo $REF_NAME
 echo $package
 
 echo "Looping through keys and values:"
+echo "oee"
 
-command="changeset version"
+command="npx changeset version"
 
 for pair in $mapping; do
     key="${pair%%:*}"
     value="${pair#*:}"
-    script
 
     if [[ "$key" != "$package" ]]; then
         command+=" --ignore $organization/$value" 
     fi
 done
 
-eval $command
+echo $command 
+
+eval "$command"
+
