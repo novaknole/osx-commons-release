626089757e872f97c7ea8485f30f2209c05cd796
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-15 12:41:25 +0400

    ss

---
+++ temp.js
@@ -0,0 +1,74 @@
+{
+  "changesets": [
+    {
+      "releases": [
+        {
+          "name": "@glagh/commons-configs",
+          "type": "minor"
+        }
+      ],
+      "summary": "configs 1",
+      "id": "bright-knives-move"
+    },
+    {
+      "releases": [
+        {
+          "name": "@glagh/commons-configs",
+          "type": "minor"
+        }
+      ],
+      "summary": "configs 2",
+      "id": "plenty-eagles-help"
+    },
+    {
+      "releases": [
+        {
+          "name": "@glagh/commons-sdk",
+          "type": "minor"
+        }
+      ],
+      "summary": "sdk 1",
+      "id": "rare-pumpkins-sip"
+    },
+    {
+      "releases": [
+        {
+          "name": "@glagh/commons-contracts",
+          "type": "minor"
+        }
+      ],
+      "summary": "contracts 1",
+      "id": "strong-ducks-sparkle"
+    }
+  ],
+  "releases": [
+    {
+      "name": "@glagh/commons-configs",
+      "type": "minor",
+      "oldVersion": "0.7.0",
+      "changesets": [
+        "bright-knives-move",
+        "plenty-eagles-help"
+      ],
+      "newVersion": "0.8.0"
+    },
+    {
+      "name": "@glagh/commons-sdk",
+      "type": "minor",
+      "oldVersion": "0.0.1",
+      "changesets": [
+        "rare-pumpkins-sip"
+      ],
+      "newVersion": "0.1.0"
+    },
+    {
+      "name": "@glagh/commons-contracts",
+      "type": "minor",
+      "oldVersion": "1.4.0",
+      "changesets": [
+        "strong-ducks-sparkle"
+      ],
+      "newVersion": "1.5.0"
+    }
+  ]
+}
